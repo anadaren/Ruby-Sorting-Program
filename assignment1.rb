31fe87ff7c566d4259906e76ce449c67a0c8bcf9
Ana Green <greena22@tcnj>
2020-09-12 15:57:46 -0400

    Input Data Working

--- assignment1.rb
+++ assignment1.rb
@@ -1,46 +1,95 @@
 # creates method to fuction as menu
 def whatDo
-puts "What do you want to do?"
-puts "1 - Open File"
-puts "2 - Edit Data"
-puts "3 - Form Groups"
-puts "4 - List Groups"
-puts "5 - Write Data to file"
-puts "6 - Quit"
+    puts "What do you want to do?"
+    puts "1 - Open File"
+    puts "2 - Edit Data"
+    puts "3 - Form Groups"
+    puts "4 - List Groups"
+    puts "5 - Write Data to file"
+    puts "6 - Quit"
 
-action = gets.chomp
+    action = gets.chomp
+    return action
 end
 
 #cycles through options until 'Quit' is selected
 loop do
-    whatDo()
+    action = whatDo()
 
-    if action == 1
-        print "Enter name of file you want to access. "
+    #input data
+    if action == '1'
+        print "Enter name of file you want to access."
         fileName = gets.chomp
         file = File.open(fileName)
+       
+        lineCount = `wc -l "#{fileName}"`.strip.split(' ')[0].to_i
+
+        file_data = Array.new(3){Array.new(lineCount)}
+
+        file_data = File.read(fileName).split(",").map(&:chomp)
+        
+        file.close
+        puts "File read into array."
     end
-    if action == 2
+    #edit data
+    if action == '2'
+        puts "What do you want to do?"
+        puts "1 - Add New Student"
+        puts "2 - Delete Student"
+        puts "3 - Edit Student Information"
+        edit = gets.chomp
 
+        if edit == '1'
+            puts "Enter new Student Information."
+            #add to array
+
+        end
+        if edit == '2'
+            puts "Enter name of Student to Delete."
+            studName = gets.chomp
+            #studentFound = search(studentName)
+            #delete student
+            #make sure array is fine
+
+        end
+        if edit == '3'
+            puts "Enter name of Student you wish to edit."
+            studName = gets.chomp
+            #studLocation = search(studName)
+            puts "Which category do you wish to change?"
+            puts "1 - First name"
+            puts "2 - Last name"
+            puts "3 - Email Address"
+            puts "4 - Primary Major"
+            puts "5 - Secondary Major"
+            puts "6 - First Minor"
+            puts "7 - Second Minor"
+            makeEdit = gets.chomp
+            puts "Enter new information."
+            file_data[studLocation][makeEdit-1] = makeEdit
+            puts "New information taken."
+        end
 
     end
-
-    if action == 3
-
-    end
-
-    if action == 4
+    #form groups
+    if action == '3'
+        puts "Enter size of groups."
+        groupSize = gets.chomp
 
     end
-    
-    if actioin == 5
-
+    #edit groups
+    if action == '4'
 
     end
-
-    if action == 6
-        File.close(file)
+    #write groups to file
+    if action == '5'
+        print "Enter name of file you want to write to."
+        file = File.open(fileName, "w")
+        File.write(fileName, [1,2,3].join("\n"), mode: "a")
+        puts "Groups written to file."
+    end
+    #quit
+    if action == '6'
         break;
     end
-
 end
