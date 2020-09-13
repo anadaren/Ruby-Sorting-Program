5c00432ebdc29411d7b1056b5ab6735965228ecb
Ana Green <greena22@tcnj>
2020-09-13 01:41:30 -0400

    Added more detailed outline

--- assignment1.rb
+++ assignment1.rb
@@ -26,8 +26,10 @@ loop do
         file_data = File.read(fileName).split(",").map(&:chomp)
         #closes file
         file.close
+        puts file_data
         puts "File read into array."
     end
+
     #edit data
     if action == '2'
         puts "What do you want to do?"
@@ -39,52 +41,97 @@ loop do
         if edit == '1'
             puts "Enter new Student Information."
             #add to array
+            studLocation = file_data.assoc(studMail)
+            if studLocation == 'nil'
+                puts "Student added to file."
+            else
+                puts "Student already in file."
+            end
 
         end
         if edit == '2'
-            puts "Enter name of Student to Delete."
-            studName = gets.chomp
-            #studentFound = search(studentName)
-            #delete student
+            puts "Enter email of Student to Delete."
+            studMail = gets.chomp
+            studLocation = file_data.assoc(studMail)
+            if studLocation == 'nil'
+                puts "Student not found."
+            else
             #make sure array is fine
-
+                puts "Student deleted."
+            end
         end
         if edit == '3'
-            puts "Enter name of Student you wish to edit."
-            studName = gets.chomp
-            #studLocation = search(studName)
-            puts "Which category do you wish to change?"
-            puts "1 - First name"
-            puts "2 - Last name"
-            puts "3 - Email Address"
-            puts "4 - Primary Major"
-            puts "5 - Secondary Major"
-            puts "6 - First Minor"
-            puts "7 - Second Minor"
-            makeEdit = gets.chomp
-            puts "Enter new information."
-            file_data[studLocation][makeEdit-1] = makeEdit
-            puts "New information taken."
+            puts "Enter email of Student you wish to edit."
+            studMail = gets.chomp
+            studLocation = file_data.assoc(studMail)
+            if studLocation == 'nil'
+                puts "Student not found."
+            else
+                puts "Which category do you wish to change?"
+                puts "1 - First name"
+                puts "2 - Last name"
+                puts "3 - Email Address"
+                puts "4 - Primary Major"
+                puts "5 - Secondary Major"
+                puts "6 - First Minor"
+                puts "7 - Second Minor"
+                makeEdit = gets.chomp
+                puts "Enter new information."
+                newInfo = gets.chomp
+                file_data[studLocation][makeEdit-1] = newInfo
+                puts "New information taken."
+            end
         end
 
     end
+
     #form groups
     if action == '3'
         puts "Enter size of groups."
         groupSize = gets.chomp
 
+        puts "How do you want the groups to be sorted? (You may enter multiple.)"
+        puts "1 - Alphabetical by last name"
+        puts "2 - Similar Majors"
+        puts "3 - Different Majors"
+        puts "4 - Section"
+
+        # take input
+
+        #sort groups
+        if constra == '1'
+            #sort by last name
+        end
+        if constra == '2'
+            #sort by contains major
+        end
+        if constra == '3'
+            #sort by different major
+        end
+        if constra == '4'
+            #sorts by section
+        end
+        #groupArray of new groups
+
+        #print out groups
+        #print
     end
+
     #edit groups
     if action == '4'
 
     end
+
     #write groups to file
     if action == '5'
         print "Enter name of file you want to write to."
-        file = File.open(fileName, "w")
-        File.write(fileName, [1,2,3].join("\n"), mode: "a")
+        writeName = gets.chomp
+        File.open(writeName, "w+") do |f|
+            f.puts(file_data)
+        end
         puts "Groups written to file."
     end
+
     #quit
     if action == '6'
         break;
